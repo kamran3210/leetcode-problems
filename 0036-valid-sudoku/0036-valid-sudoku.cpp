764dcd250875118318e07fa916e6a313c86920e0
Kamran Ahmed <kamran322@hotmail.co.uk>
2023-06-24 13:07:13 +0100

    Time: 20 ms (83.67%), Space: 17.8 MB (92.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,49 +1,23 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            if (!isValidRow(i, board) || !isValidCol(i, board) || !isValidBox(i, board))
-                return false;
-        }
-        return true;
-    }
-private:
-    bool isUnique(char c, bool set[]) {
-        if (c == '.') return true;
-        if (set[c - '1']) return false;
-        return set[c - '1'] = true;
-    }
-
-    bool isValidRow(int row, vector<vector<char>>& board) {
-        bool set[9];
-        memset(set, false, sizeof(set));
-        for (auto const& c : board[row]) {
-            if (!isUnique(c, set)) return false;
-        }
-        return true;
-    }
-
-    bool isValidCol(int col, vector<vector<char>>& board) {
-        bool set[9];
-        memset(set, false, sizeof(set));
-        for (int i = 0; i < 9; i++) {
-            char c = board[i][col];
-            if (!isUnique(c, set)) return false;
-        }
-        return true;
-    }
-
-    bool isValidBox(int box, vector<vector<char>>& board) {
-        bool set[9];
-        memset(set, false, sizeof(set));
-        int x = (box % 3) * 3;
-        int y = (box / 3) * 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                char c = board[y + i][x + j];
-                if (!isUnique(c, set)) return false;
-            }
-        }
+    bool isValidSudoku(vector<vector<char> > &board)
+    {
+        bool rowSets[9][9] = {0};
+        bool colSets[9][9] = {0}; 
+        bool boxSets[9][9] = {0};
+        
+        for(int row = 0; row < 9; row++)
+            for(int col = 0; col < 9; col++)
+                if(board[row][col] != '.')
+                {
+                    int num = board[row][col] - '1';
+                    int box = row / 3 * 3 + col / 3;
+                    if(rowSets[row][num] || colSets[col][num] || boxSets[box][num])
+                        return false;
+                    rowSets[row][num] = colSets[col][num] = boxSets[box][num] = true;
+                }
+        
         return true;
     }
 };
